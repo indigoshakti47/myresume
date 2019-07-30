0e5b8270d7dba8897a16dd302c00ff3da9ad5d79
Nathalia <indigodreamer11@hotmail.com>
2019-07-29 21:42:27 -0500

    Navigation menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Layout, Header, Navigation, Drawer, Title, Content} from 'react-mdl';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div style={{height: '300px', position: 'relative'}}>
+    <Layout fixedHeader style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+    <Header title={<span><span style={{ color: '#ddd' }}>Nathalia / </span><strong>Indigo</strong></span>}>
+            <Navigation>
+                <a href="#">About Me</a>
+                <a href="#">Achievements</a>
+                <a href="#">Skills/Projects</a>
+                <a href="#">Social</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">About Me</a>
+                <a href="#">Achievements</a>
+                <a href="#">Skills/Projects</a>
+                <a href="#">Social</a>
+            </Navigation>
+        </Drawer>
+        <Content />
+    </Layout>
+</div>
+    
   );
 }
 
