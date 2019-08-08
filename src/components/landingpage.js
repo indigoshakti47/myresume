4a62c105b19e6a5d896ea9f791f7a41ed4fe1d88
Nathalia <indigodreamer11@hotmail.com>
2019-08-07 23:28:23 -0500

    Layout minor change

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import {Grid, Cell} from 'react-mdl';
 
 class LandingPage extends Component{
     render(){
         return(
-            <div>
-                <h1> Main </h1>
+            <div style={{width:'100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                    <img src="http://fiyazhasan.me/content/images/2017/06/default-avatar-png.png" alt="avatar" className="my-avatar"></img>
+                    <div className="my-text">
+                        <h2 style={{alignContent:'center'}}>Visionary tech entrepreneur</h2>
+                        <hr></hr>
+                        <p> Disruptive engineer</p>
+                        <p>
+                         Envisioning new worlds | Starting projects | Networking | Traveling 
+                        </p>
+
+                    </div>
+                    </Cell>
+
+
+                </Grid>
 
 
             </div>
--- src/components/social.js
+++ src/components/social.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react'
 class Social extends Component{
     render(){
         return(
-            <div>
+            <div className="social-links">
+                <a href=""></a>
                 <h1> Social Networks </h1>
 
 
